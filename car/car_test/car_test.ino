9198a6afc91d815c0161659354e5e023ae6f36d7
Backback12 <cepagtakhan@gmail.com>
2024-10-26 13:10:09 -0500

    General Update

--- car/car_test/car_test.ino
+++ car/car_test/car_test.ino
@@ -2,6 +2,7 @@
 #include <Servo.h>
 #include <AccelStepper.h>
 #include <Adafruit_MotorShield.h>
+#include <Arduino_APDS9960.h>
 
 
 Adafruit_MotorShield AFMSbot(0x61); // included in example and it worked
@@ -20,42 +21,119 @@ Servo servo_B;
 // you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
 // wrappers for the first motor!
 void forwardstep1() {
-  myStepper1->onestep(FORWARD, SINGLE);
+  stepper_A->onestep(FORWARD, SINGLE);
 }
 void backwardstep1() {
-  myStepper1->onestep(BACKWARD, SINGLE);
+  stepper_A->onestep(BACKWARD, SINGLE);
 }
 // wrappers for the second motor!
 void forwardstep2() {
-  myStepper2->onestep(FORWARD, DOUBLE);
+  stepper_B->onestep(FORWARD, DOUBLE);
 }
 void backwardstep2() {
-  myStepper2->onestep(BACKWARD, DOUBLE);
+  stepper_B->onestep(BACKWARD, DOUBLE);
 }
 
-AccelStepper stepper1(forwardstep1, backwardstep1);   // assign wrappers
-AccelStepper stepper2(forwardstep2, backwardstep2);
+AccelStepper stepperA(forwardstep1, backwardstep1);   // assign wrappers
+AccelStepper stepperB(forwardstep2, backwardstep2);
 
+// ********************************************** SERVO WRAPPER FUNCTIONS ****************************
+const int OFFSET_A = 2; // offset to match servo's "zero point"
+const int OFFSET_B = 1; // offset to match servo's "zero point"
+
+// -90 = CCW
+//  0  = NOTHING
+//  90 = CLOCKWISE
+void turnServo(Servo target, int amount) {
+//  if (target == servo_A) {
+//    target.write(OFFSET_A + amount + 90); 
+//  }
+//  else {
+//    target.write(OFFSET_B + amount + 90);
+//  }
+}
+
+void setServoA(int deg) {
+  
+}
+
+// ********************************************** COLOUR WRAPPER FUNCTIONS ****************************
+// OUTPUT
+//    -1 INVALID
+//     0 NOT RED
+//     1 RED
+int isRed() {
+  Serial.begin(9600);
+  
+  // check if colour sensor returns "red"
+  int r, g, b;
+  if (APDS.colorAvailable()) {
+    APDS.readColor(r, g, b);
+
+    // R > 15, G < 15, B < 15
+
+    // (G < 0.6 * R) and (B < 0.6 * R)
+    if (g < 0.6 * r && b < 0.6 * r) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+  else {
+    return -1;
+  }
+}
+
+
+
+
+
+// ****************************************************************************************************
 
 void setup() {
   servo_A.attach(10);  // attaches the servo on pin 9 to the servo object
   servo_B.attach(9);  // attaches the servo on pin 9 to the servo object
 
+//  turnServo(servo_A, 0);
+//  turnServo(servo_B, 0);
+  servo_A.write(90 + 2);
+  servo_B.write(90 + 1);
+  
   
   // start top shield, start bottom shield?
 //  AFMSbot.begin(); // Start the bottom shield
   AFMStop.begin(); // Start the top shield
 
 
-  stepper1.setMaxSpeed(100.0);
-  stepper1.setAcceleration(100.0);
-  stepper1.moveTo(24);
+  stepperA.setMaxSpeed(100.0);
+  stepperA.setAcceleration(100.0);
+  stepperA.moveTo(24);
 
-  stepper2.setMaxSpeed(200.0);
-  stepper2.setAcceleration(100.0);
-  stepper2.moveTo(50000);
+  stepperB.setMaxSpeed(200.0);
+  stepperB.setAcceleration(100.0);
+  stepperB.moveTo(50000);
+
+  
+  // START COLOUR SENSOR
+  if (!APDS.begin()) {
+    Serial.println("Error initializing APDS9960 sensor!");
+  }
+  APDS.setLEDBoost(3);
+  
 }
 
 void loop() {
+//  int x;
+//  x = isRed();
+//  if (x != -1) {
+//    Serial.print("VAL: ");
+//    Serial.println(x);
+//  }
+//
+//  delay(50);
+
+
+
   
 }
--- test_programs/ColourSensor/ColourSensor.ino
+++ test_programs/ColourSensor/ColourSensor.ino
@@ -11,18 +11,25 @@ void setup() {
     Serial.println("Error initializing APDS9960 sensor!");
   }
   APDS.setLEDBoost(3);
+
+
+  Serial.println("R:,G:,B:,CUTOFF");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (APDS.colorAvailable()) {
     APDS.readColor(r, g, b);
-    Serial.print("Red: ");
-    Serial.println(r);
-    Serial.print("Green: ");
-    Serial.println(g);
-    Serial.print("Blue: ");
-    Serial.println(b);
+//    Serial.print("Red: ");
+    Serial.print(r);
+    Serial.print(", ");
+    Serial.print(g);
+    Serial.print(", ");
+    Serial.print(b);
+    
+    // cutoff = 60% of RED
+    Serial.print(", ");
+    Serial.println(r * 0.6);
   }
-  delay(500);
+  delay(50);
 }
